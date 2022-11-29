202767f3ac7eb54a6c84858743bd5b35005f3374
Junho Cho <78071893+hiwhwnsgh@users.noreply.github.com>
2022-11-29 22:36:12 +0900

    Add Info GUI
    GUI 추가 및 수정

---
+++ Real_Estate_DB_Project/src/Login/Start.java
@@ -0,0 +1,19 @@
+package Login;
+
+import java.awt.*;
+
+public class Start {
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					new Login();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
